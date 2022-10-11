298607cfb9ba5f7b6d2654f3e4c32c4cce15b0b7
Althaf Ahammed M <60294370+Althaf-ahammed@users.noreply.github.com>
2022-10-11 18:36:51 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3'
-export const API_KEY = '5cba877a6e176cf02a801c92e8e9498e'
+export const API_KEY = 'API key'
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
